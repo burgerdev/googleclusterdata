ebf0be2a216963ce0b91f31f859dae0f412ae50b
Markus Döring <webmaster@burgerdev.de>
2015-07-22 22:29:09 +0200

    gave schema its own module to reuse it for filling tables

--- apply_schema.py
+++ apply_schema.py
@@ -1,83 +1,33 @@
-import sys
-import csv
-from pprint import pprint
 
-from itertools import groupby
+from schema import Table, get_table_names
 
+import logging
+
+logger = logging.getLogger("apply_schema")
+logging.basicConfig(level=logging.INFO)
 
 disclaimer = """
 Make sure that your input schema file is sanitized!
 """
 
-class _TypeError(Exception):
-    pass
 
-
-def type_map(t):
-    if t == "STRING_HASH":
-        return "VARCHAR(512)"
-    if t == "STRING_HASH_OR_INTEGER":
-        raise _TypeError("Don't know how to interpret that")
-    return t
-
-
-def extract_table_name(row):
-    path = row[0]
-    name = path.split("/")[0]
-    return name
-
-
-def extract_index(row):
-    return int(row[1])
-
-
-def extract_type(row):
-    return type_map(row[3])
-
-
-def extract_mandatory(row):
-    return row[4] == "YES"
-
-
-def extract_col_name(row):
-    repl = [(' ', '_'),
-            ("user", "username"),
-            ("/", "")]
-    s = row[2]
-    for a, b in repl:
-        s = s.replace(a, b)
-    return s
-
-
-def describe_single_col(row):
-    d = dict()
-    d["name"] = extract_col_name(row)
-    d["type"] = extract_type(row)
-
-    d["extras"] = ""
-
-    if extract_mandatory(row):
-        d["extras"] += " NOT NULL"
-
-    return "{name} {type}{extras}".format(**d)
-
-
-def run(conn, f):
-    reader = csv.reader(f)
-    header = reader.next()
-
-    grouped = groupby(reader, extract_table_name)
-
+def run(conn):
     with conn.cursor() as c:
-
-        for name, g in grouped:
+        for name in get_table_names():
+            table = Table(name)
             try:
-                d = dict(name=name, desc=", ".join(map(describe_single_col, g)))
-            except _TypeError:
-                print("skipping table '{}' with strange types".format(name))
-            cmd = "DROP TABLE IF EXISTS {name};".format(**d)
+                desc = map(lambda col: col.describe(), table)
+            except ValueError:
+                logger.warn("skipping table '{}' with "
+                            "strange types".format(name))
+                continue
+
+            logger.info("dropping table '{}', if it exists".format(name))
+            cmd = "DROP TABLE IF EXISTS {};".format(name)
             c.execute(cmd)
-            cmd = "CREATE TABLE {name} ({desc});".format(**d)
+
+            logger.info("creating table '{}', if it exists".format(name))
+            cmd = "CREATE TABLE {} ({});".format(name, ", ".join(desc))
             c.execute(cmd)
 
 
@@ -88,9 +38,6 @@ if __name__ == "__main__":
     parser.add_argument("-y", "--disclaimer_accepted", action="store_true",
                         default=False,
                         help="I have read the disclaimer")
-    parser.add_argument("-f", "--file", action="store", type=file,
-                        default=sys.stdin,
-                        help="location of schema.csv (default: stdin)")
     args = parser.parse_args()
     print(disclaimer)
 
@@ -100,7 +47,7 @@ if __name__ == "__main__":
         if not args.disclaimer_accepted:
             print("re-run with -y if your input is sanitized.")
         else:
-            run(conn, args.file)
+            run(conn)
     except:
         raise
     else:
---
+++ schema.py
@@ -0,0 +1,106 @@
+
+import os
+import csv
+from collections import defaultdict
+
+from config import config
+
+
+_basedir = config.get("clusterdata", "root")
+
+with open(os.path.join(_basedir, "schema.csv"), 'r') as _schemafile:
+    _reader = csv.reader(_schemafile)
+    _reader.next()
+    _content = list(_reader)
+
+
+_grouped = defaultdict(list)
+for item in _content:
+    key = item[0].split('/')[0]
+    _grouped[key].append(item)
+
+_table_names = _grouped.keys()
+
+
+def get_table_names():
+    return _table_names
+
+
+class Table(object):
+    """
+    TODO doc
+    """
+
+    def __init__(self, table_name):
+        assert table_name in _table_names,\
+            "unknown table {}".format(table_name)
+        self._name = table_name
+        self._cols = map(Column, _grouped[table_name])
+
+    def get_columns(self):
+        return self._cols
+
+    def __len__(self):
+        return len(self._cols)
+
+    def __getitem__(self, i):
+        return self._cols[i]
+
+    def __iter__(self):
+        for c in self._cols:
+            yield c
+
+
+class Column(object):
+    """
+    TODO doc
+    """
+
+    def __init__(self, row):
+        self._row = row
+
+    def describe(self):
+        return self._describe_single_col(self._row)
+
+    @staticmethod
+    def _type_map(t):
+        if t == "STRING_HASH":
+            return "VARCHAR(512)"
+        if t == "STRING_HASH_OR_INTEGER":
+            raise ValueError("Don't know how to interpret that")
+        return t
+
+    @staticmethod
+    def _extract_index(row):
+        return int(row[1])
+
+    @staticmethod
+    def _extract_type(row):
+        return Column._type_map(row[3])
+
+    @staticmethod
+    def _extract_mandatory(row):
+        return row[4] == "YES"
+
+    @staticmethod
+    def _extract_col_name(row):
+        repl = [(' ', '_'),
+                ("user", "username"),
+                ("/", "")]
+        s = row[2]
+        for a, b in repl:
+            s = s.replace(a, b)
+        return s
+
+    @staticmethod
+    def _describe_single_col(row):
+        d = dict()
+        d["name"] = Column._extract_col_name(row)
+        d["type"] = Column._extract_type(row)
+
+        d["extras"] = ""
+
+        if Column._extract_mandatory(row):
+            d["extras"] += " NOT NULL"
+
+        return "{name} {type}{extras}".format(**d)
